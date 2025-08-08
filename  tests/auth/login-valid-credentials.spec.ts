a55f1e8648acee9d70802b5db349de4cb0a7305b
Karina Karpinskaya <karinakarpinskaya72@gmail.com>
2025-08-08 23:26:09 +0300

    Add login valid credentials test

---  tests/auth/login-valid-credentials.spec.ts
+++  tests/auth/login-valid-credentials.spec.ts
@@ -3,9 +3,12 @@ import { test, expect } from "@playwright/test";
 test("Verify login with valid credentials", async ({ page }) => {
   await page.goto("/auth/login");
 
-  await page.getByTestId("email").fill("customer@practicesoftwaretesting.com");
-  await page.getByTestId("password").fill("welcome01");
-  await page.getByTestId("login-submit").click();
+  await page.fill(
+    '[data-test="email"]',
+    "customer@practicesoftwaretesting.com"
+  );
+  await page.fill('[data-test="password"]', "welcome01");
+  await page.click('[data-test="login-submit"]');
 
   await expect(page).toHaveURL("/account");
 
